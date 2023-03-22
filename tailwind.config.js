34c9f6af282cc8632d09d6d868c4a09966a5d05d
abubakkar-js-dev <mdabubakkars182@gmail.com>
2023-03-22 15:39:05 +0600

    cusmtozed the tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
     content: ['./**/*.html', './pages/**/*.{html, js}'],
     darkMode: "class",
     theme: {
-        extend: {},
+        screen: {
+            sm: '480px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px'
+        },
+        color: {
+            blue: '#1fb6ff',
+            pink: '#ff49db',
+            orange: '#ff7849,'
+
+        },
+        fontFamil: {
+            sans: ['Graphik', 'sans-serif'],
+            arial: ['arial','arial-black']
+        },
+        extend: {
+            spacing: {
+                '128':'32rem',
+                '144': '36rem'
+            },
+            borderRadiud: {
+                '4xl': '2rem'
+            }
+        },
     },
-    plugins: [],
+
 }
